8ff403661059bbcef881b8ec9106ad27567449df
Andrew Moulton James <amjames2@vt.edu>
2015-12-01 16:36:19 -0500

    found that error line bug

--- bash/f.git-prompt-colors.sh
+++ bash/f.git-prompt-colors.sh
@@ -51,7 +51,7 @@ override_git_prompt_colors(){
     
     
     GIT_PROMPT_COMMAND_OK="${Green}✔ ${ResetColor}[${BoldMagenta}${USER}${ResetColor}@${BoldBlue}${HOSTNAME%%.*}${ResetColor}]"
-    GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_${ResetColor}[${BoldMagenta}${USER}${ResetColor}@${BoldBlue}${HOSTNAME%%*}${ResetColor}]"
+    GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_${ResetColor}[${BoldMagenta}${USER}${ResetColor}@${BoldBlue}${HOSTNAME%%.*}${ResetColor}]"
     GIT_PROMPT_START_ROOT="<${BoldRed}ROOT${ResetColor}>"
 
 }
