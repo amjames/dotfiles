5eb2d04ea4601dd5d0bd844939085aefcbb315ca
Andrew Moulton James <amjames2@vt.edu>
2016-02-15 14:57:38 -0500

    added mpd and ncmpcpp configs to repo

---
+++ music/d.mpd/playlists/internet_radio_stream_aliases.sh
@@ -0,0 +1,18 @@
+# 1. Install mplayer command line (via Brew, Macports, or APT)
+# 2. Add the following aliases to ~/.profile
+# 3. Type `source ~/.profile`
+# 3. Type `news` or `current` to listen in your terminal
+
+# curl http://minnesota.publicradio.org/tools/play/streams/news.pls               > news.m3u
+# curl http://minnesota.publicradio.org/tools/play/streams/the_current.pls        > current.m3u
+# curl http://minnesota.publicradio.org/tools/play/streams/classical.pls          > classical.m3u
+# curl http://minnesota.publicradio.org/tools/play/streams/local.pls              > localcurrent.m3u
+# curl http://minnesota.publicradio.org/tools/play/streams/radio_heartland.pls    >heartland.m3u
+# curl http://wondergroundstream2.publicradio.org/wonderground >wonder-ground.m3u
+# curl http://choralstream1.publicradio.org/choral.m3u > choral.m3u
+# curl http://www.wefunkradio.com/play/shoutcast.pls > weFunk.m3u
+# curl http://sleepbot.com/ambience/cgi/listen.cgi/listen.pls > sleep-bot.m3u
+curl http://somafm.com/groovesalad130.pls >grooveSalad.m3u
+curl http://somafm.com/dronezone130.pls > drone-zone.m3u
+curl http://somafm.com/lush130.pls > lush.m3u
+curl http://somafm.com/sonicuniverse.pls > sonic-universe.m3u
