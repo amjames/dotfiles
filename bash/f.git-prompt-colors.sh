ca17643ee24c318e6d8da04cb727547ad21ee5b1
Andrew Moulton James <amjames2@vt.edu>
2016-03-14 08:57:08 -0400

    update prompt

--- bash/f.git-prompt-colors.sh
+++ bash/f.git-prompt-colors.sh
@@ -56,8 +56,6 @@ function prompt_callback {
 
 override_git_prompt_colors(){
     GIT_PROMPT_THEME_NAME="Custom"
-    
-    
     GIT_PROMPT_COMMAND_OK="${Green}✔ ${ResetColor}[${BoldMagenta}${USER}${ResetColor}@${BoldBlue}${HOSTNAME%%.*}${ResetColor}]"
     GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_${ResetColor}[${BoldMagenta}${USER}${ResetColor}@${BoldBlue}${HOSTNAME%%.*}${ResetColor}]"
     GIT_PROMPT_START_ROOT="<${BoldRed}ROOT${ResetColor}>"
