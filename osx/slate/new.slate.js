a12dabb7474d606dfae7a27f0e9d41891923c3f9
Andrew Moulton James <amjames2@vt.edu>
2016-04-20 08:44:10 -0400

    slate updates

--- osx/slate/new.slate.js
+++ osx/slate/new.slate.js
@@ -3,8 +3,8 @@
  * Project: http://github.com/lunixbochs/reslate
  */
 S.log('Loading Config .... ');
-S.src('~/Workspace/Git/dotfiles/osx/slate/reslate/reslate.js');
 S.src('~/Workspace/Git/dotfiles/osx/slate/defines.js');
+S.src('~/Workspace/Git/dotfiles/osx/slate/reslate/reslate.js');
 // enable to see debug messages in Console.app
 $.debug = true;
 
@@ -88,23 +88,34 @@ slate.bindAll({
         '2': $('toss', '1', 'resize'),
         '3': $('toss', '2', 'resize'),
         // direct focus
-        a: $.focus('Asana'),
-        c: $.focus('Messages'),
-        s: $.focus('Safari'),
-        t: $.focus('Terminal'),
-        f: $.focus('Wunderlist'),
-        e: $.focus('Airmail'),
-        p: $.focus('iTunes'),
-        x: $.focus('SourceTree')
         // utility functions
         f1: 'relaunch',
         z: 'undo',
-        tab: 'hint'
+        q: 'grid'
     },
   lowper: {
     shift:  {
-      q: 'grid'
-    }
+      //I want to replace these with "open new window osascript hacks"
+      a: $.focus('Asana'),
+      c: $.focus('Messages'),
+      s: $.focus('Safari'),
+      t: $.focus('Terminal'),
+      f: $.focus('Wunderlist'),
+      e: $.focus('Airmail'),
+      p: $.focus('iTunes'),
+      x: $.focus('SourceTree')
+    },
+      '`': 'hint',
+      //replace these with {if app open --> focus app;} {else--> open aopp; focus app }
+      //type statements
+      a: $.focus('Asana'),
+      c: $.focus('Messages'),
+      s: $.focus('Safari'),
+      t: $.focus('Terminal'),
+      f: $.focus('Wunderlist'),
+      e: $.focus('Airmail'),
+      p: $.focus('iTunes'),
+      x: $.focus('SourceTree')
   }
 });
 
