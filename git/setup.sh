d30be59785c9ac48fd4be00e5d592880b416cfdb
Andrew M. James <amjames2@vt.edu>
2018-04-25 11:38:03 -0400

    [GIT] modifed setup, forsome reason template dir relative to include file did not work

--- git/setup.sh
+++ git/setup.sh
@@ -1,9 +1,12 @@
 # get the path to this script
 git_cfg_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "setting include.path to ${git_cfg_dir}"
+echo "setting init.templatedir to ${git_cfg_dir}/git_template"
+echo "setting core.excludesfile to ${git_cfg_dir}/gitignore_global"
+git config --global include.path "${git_cfg_dir}/gitconfig"
+git config --global init.templatedir "${git_cfg_dir}/git_template"
+git config --global core.excludesfile "${git_cfg_dir}/gitignore_global"
 
-
-echo "[include]" > ~/.gitconfig
-echo "    path = ${git_cfg_dir}/gitconfig" >> ~/.gitconfig
 
 #cleanup
 unset -v git_cfg_dir
